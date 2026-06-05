1b911527c6f83a41399abacae2fb9d305ee957bf
Karthick Raja <mkkarthickraja07@gmail.com>
2026-06-05 21:12:39 +0530

    Deleted files in TreesProblems

--- Leetcode/TreesProblems/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello :) This folder consist of the problems i've solved during my interview preparation.");
-        System.out.println();
-        System.out.println("There is no problem if you copy. But Revise the approach of each problem and know the patterns.");
-    }
-}
